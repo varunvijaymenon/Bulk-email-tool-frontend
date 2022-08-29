9ed2e6e8e9dbc35ba6ca665caaed1a684e95df64
varun vijay <varunvijaymenon@gmail.com>
2022-08-29 12:25:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import './App.css'
 
 function App() {
+  const [recepients, setRecepients] = React.useState('');
+  const [message, setMessage] = React.useState('');
+  const [subject, setSubject] = React.useState('');
+
+  const send_email = () => {
+    fetch('http://localhost:4000/send', {method: 'POST', headers: {'Content-Type': 'application/json'}, body:JSON.stringify({emails: recepients, subject:subject, message: message})})}
+  //   console.log(recepients)
+  // console.log(message) 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Bulk Email Sender using Node</h3>
+      <div className="email-container">
+        <TextField
+            id="standard-helperText"
+            label="To"
+            helperText="comma seperated email ids"
+            variant="standard"
+            value = {recepients}
+            sx ={{ width : '100%'}}
+            onChange={e => setRecepients(e.target.value)}
+          />
+          <br/>
+          <TextField
+            id="standard-helperText"
+            label="Subject"
+            helperText=""
+            variant="standard"
+            value = {subject}
+            sx ={{ width : '100%'}}
+            onChange={e => setSubject(e.target.value)}
+          />
+          <br/>
+          <TextField
+            id="outlined-multiline-static"
+            label="Message body"
+            multiline
+            rows={10}
+            sx ={{ width : '100%'}}
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          />
+          <br/>
+        <Button variant="contained" onClick= {() => send_email()} >send</Button>
+      </div>
     </div>
   );
 }
